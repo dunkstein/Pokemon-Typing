42dbbc81a01c125fef48476ab049d8ece18ad331
VeryAwfulRotten <92930040+VeryAwfulRotten@users.noreply.github.com>
2021-11-01 10:06:56 -0400

    NewTitle

--- trashPokemon/titlePage.java
+++ trashPokemon/titlePage.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class titlePage extends World
 {
     Label titleLabel = new Label("Trash Pokemon", 100);
+    Label startLabel = new Label("Press <space> to Start", 100);
     /**
      * Constructor for objects of class titlePage.
      * 
@@ -16,9 +17,10 @@ public class titlePage extends World
     public titlePage()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(16*60, 9*60, 1);
+        super(960, 540, 1);
         Greenfoot.start();
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(startLabel, getWidth()/2, (getHeight()/4)*3);
     }
     public void act()
     {
