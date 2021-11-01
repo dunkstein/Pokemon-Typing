0ba919f7fdc3aa110e7cd94a44f7602407748b8f
335497137 <335497137@gapps.yrdsb.ca>
2021-11-01 09:32:26 -0400

    title page

--- trashPokemon/titlePage.java
+++ trashPokemon/titlePage.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class titlePage extends World
 {
-
+    Label titleLabel = new Label("Trash Pokemon", 100);
     /**
      * Constructor for objects of class titlePage.
      * 
@@ -18,9 +18,14 @@ public class titlePage extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         Greenfoot.start();
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
     }
     public void act()
     {
-        if (Greenfoot.isKeyDown("space")) Greenfoot.setWorld(new MyWorld());
+        //Start game if space bar is pressed
+        if (Greenfoot.isKeyDown("space")){
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
